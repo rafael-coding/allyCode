1f22deb3da7c42c2c2e162cb7f01530271cb8791
Rafael de Abreu Ferreira <rafael.abreu.ferreira10@gmail.com>
2021-05-17 11:58:18 -0300

    adicionando display none (na versão desktop)  no botão que fecha o calendário

--- js/main.js
+++ js/main.js
@@ -2,45 +2,45 @@
 
 /*-------------página de login----------*/
 //Elementos DOM Form
-const $login = document.querySelector("#login")
-const $password = document.querySelector("#password")
-const $inputError = document.querySelector("#inputError")
-const $inputErrorPw = document.querySelector("#inputError2")
-const $btnEnviar = document.querySelector("#btnEnviar")
+const login = document.querySelector("#login")
+const password = document.querySelector("#password")
+const inputError = document.querySelector("#inputError")
+const inputErrorPw = document.querySelector("#inputError2")
+const btnEnviar = document.querySelector("#btnEnviar")
 
 
 function validateLogin(){
-    const loginInput = $login.value
+    const loginInput = login.value
     const valid = onclick="window.location.href=logged.html"
 
     if(loginInput === ''){
-        $inputError.style.display = 'block';
-        $inputError.style.marginBottom = '10px';
-        $login.style.border = '1px solid red';
-        $login.style.marginBottom = '0px';
+        inputError.style.display = 'block';
+        inputError.style.marginBottom = '10px';
+        login.style.border = '1px solid red';
+        login.style.marginBottom = '0px';
         return false;
     } else{
-        $inputError.style.display = 'none';
-        $login.style.border = '1px solid #979797';
-        $login.style.marginBottom = '20px';
-        $btnEnviar = valid
+        inputError.style.display = 'none';
+        login.style.border = '1px solid #979797';
+        login.style.marginBottom = '20px';
+        btnEnviar = valid
         return true;
     }
 }
 
 function validatePassword(){
-    const passwordInput = $password.value
+    const passwordInput = password.value
 
     if(passwordInput === ''){
-        $inputErrorPw.style.display = 'block';
-        $inputErrorPw.style.marginBottom = '10px';
-        $password.style.border = '1px solid red';
-        $password.style.marginBottom = '0px';
+        inputErrorPw.style.display = 'block';
+        inputErrorPw.style.marginBottom = '10px';
+        password.style.border = '1px solid red';
+        password.style.marginBottom = '0px';
         return false;
     } else{
-        $inputErrorPw.style.display = 'none';
-        $password.style.border = '1px solid #979797';
-        $password.style.marginBottom = '20px';
+        inputErrorPw.style.display = 'none';
+        password.style.border = '1px solid #979797';
+        password.style.marginBottom = '20px';
         return true;
     }
 }
@@ -49,10 +49,10 @@ function validateForm(){
     validateLogin()
     validatePassword()
 
-    if(validateLogin() == ''){
+    if(validateLogin() === ''){
         return false
     }
-    if(validatePassword() ==''){
+    if(validatePassword() ===''){
         return false
     }
     return false
@@ -60,31 +60,33 @@ function validateForm(){
 
 /*----------página logado ------------*/
 
-const $navBar = document.querySelector("#navBar")
-const $iconChange = document.querySelector("#icon")
-const $closeNav = document.querySelector("#iconClose")
+
+/*  elementos para abrir e fechar a navbar   */
+const navBar = document.querySelector("#navBar")
+const iconChange = document.querySelector("#icon")
+const closeNavBar = document.querySelector("#iconClose")
 
 /*elementos para dar hover no menu*/
-const $navHome = document.querySelector("#navHome")
-const $navEncontros = document.querySelector("#navEncontros")
-const $navAgenda = document.querySelector("#navAgenda")
-const $navSair = document.querySelector("#navSair")
-const $aHome = document.querySelector("#aHome")
-const $aEncontros = document.querySelector("#aEncontros")
-const $aAgenda = document.querySelector("#aAgenda")
-const $aSair = document.querySelector("#aSair")
-const $agendar = document.querySelector("#agendar")
-const $agendar1 = document.querySelector("#agendar1")
-const $agendar2 = document.querySelector("#agendar2")
-const $agendar3 = document.querySelector("#agendar3")
-const $agendar4 = document.querySelector("#agendar4")
-const $agendar5 = document.querySelector("#agendar5")
-const $agendar6 = document.querySelector("#agendar6")
-const $agendar7 = document.querySelector("#agendar7")
-const $closeMetting = document.querySelector("#mettingClose")
-const $showMetting = document.querySelector("#mettingShow")
-const $mettingSection = document.querySelector("#metting")
-const $mettingData = document.querySelector("#mettingData")
+const navHome = document.querySelector("#navHome")
+const navEncontros = document.querySelector("#navEncontros")
+const navAgenda = document.querySelector("#navAgenda")
+const navSair = document.querySelector("#navSair")
+const aHome = document.querySelector("#aHome")
+const aEncontros = document.querySelector("#aEncontros")
+const aAgenda = document.querySelector("#aAgenda")
+const aSair = document.querySelector("#aSair")
+const agendar = document.querySelector("#agendar")
+const agendar1 = document.querySelector("#agendar1")
+const agendar2 = document.querySelector("#agendar2")
+const agendar3 = document.querySelector("#agendar3")
+const agendar4 = document.querySelector("#agendar4")
+const agendar5 = document.querySelector("#agendar5")
+const agendar6 = document.querySelector("#agendar6")
+const agendar7 = document.querySelector("#agendar7")
+
+/*   elementos para abrir e fechar a seção de agendamento  */
+const mettingSection = document.querySelector("#metting")
+const mettingData = document.querySelector("#mettingData")
 
 /*listiners para pegar onclick em todos os buttons agendar 
 const $divAgendar = document.getElementsByClassName("agendarEncontro")
@@ -99,15 +101,15 @@ const $agendar = document.getElementsByClassName("mettingShow")
 
 /* função para abrir e fechar o navbar */
 function dropNav(){
-    $navBar.style.display = "block";
-    $closeNav.style.display = "block";
-    $iconChange.style.display = 'none';
+    navBar.style.display = "block";
+    closeNavBar.style.display = "block";
+    iconChange.style.display = 'none';
 }
 
 function closeNav(){
-    $navBar.style.display = "none";
-    $closeNav.style.display = "none";
-    $iconChange.style.display = 'block';
+    navBar.style.display = "none";
+    closeNavBar.style.display = "none";
+    iconChange.style.display = 'block';
 }
 
 
@@ -116,46 +118,46 @@ function closeNav(){
 /*função para dar o hover nos itens da navbar */
 /*-----------------------hover no home */
 function hoverHome(){
-    $navHome.style.color = "#E21683";
-    $aHome.style.color = "#E21683";
+    navHome.style.color = "#E21683";
+    aHome.style.color = "#E21683";
 }
 
 function hoverOutHome(){
-    $navHome.style.color = "#404040";
-    $aHome.style.color = "#404040";
+    navHome.style.color = "#404040";
+    aHome.style.color = "#404040";
 }
 
 /*---------------------hover no Encontros */
 function hoverEncontros(){
-    $navEncontros.style.color = "#E21683";
-    $aEncontros.style.color = "#E21683";
+    navEncontros.style.color = "#E21683";
+    aEncontros.style.color = "#E21683";
 }
 
 function hoverOutEncontros(){
-    $navEncontros.style.color = "#404040";
-    $aEncontros.style.color = "#404040";
+    navEncontros.style.color = "#404040";
+    aEncontros.style.color = "#404040";
 }
 
 /*-------------------------hover no agenda */
 function hoverAgenda(){
-    $navAgenda.style.color = "#E21683";
-    $aAgenda.style.color = "#E21683";
+    navAgenda.style.color = "#E21683";
+    aAgenda.style.color = "#E21683";
 }
 
 function hoverOutAgenda(){
-    $navAgenda.style.color = "#404040";
-    $aAgenda.style.color = "#404040";
+    navAgenda.style.color = "#404040";
+    aAgenda.style.color = "#404040";
 }
 
 /*----------------------hover no sair */
 function hoverSair(){
-    $navSair.style.color = "#E21683";
-    $aSair.style.color = "#E21683";
+    navSair.style.color = "#E21683";
+    aSair.style.color = "#E21683";
 }
 
 function hoverOutSair(){
-    $navSair.style.color = "#404040";
-    $aSair.style.color = "#404040";
+    navSair.style.color = "#404040";
+    aSair.style.color = "#404040";
 }
 
 
@@ -165,84 +167,84 @@ function hoverOutSair(){
 /* estou separando os mouseovers por funcção (estudar fazer looping de divs, 8 cards é rápido, mais de 100 cards complica) */
 /* ----- onmouseouver perfil 0 --- */
 function hoverAgendar(){
-    $agendar.style.bottom = '0';
+    agendar.style.bottom = '0';
 }
 
 function hoverOutAgendar(){
-    $agendar.style.bottom = '-310px';
+    agendar.style.bottom = '-310px';
 }
 /* ----- onmouseouver perfil 1 --- */
 function hoverAgendar1(){
-    $agendar1.style.bottom = '0';
+    agendar1.style.bottom = '0';
 }
 
 function hoverOutAgendar1(){
-    $agendar1.style.bottom = '-310px';
+    agendar1.style.bottom = '-310px';
 }
 /* ----- onmouseouver perfil 2 --- */
 function hoverAgendar2(){
-    $agendar2.style.bottom = '0';
+    agendar2.style.bottom = '0';
 }
 
 function hoverOutAgendar2(){
-    $agendar2.style.bottom = '-310px';
+    agendar2.style.bottom = '-310px';
 }
 /* ----- onmouseouver perfil 3 --- */
 function hoverAgendar3(){
-    $agendar3.style.bottom = '0';
+    agendar3.style.bottom = '0';
 }
 
 function hoverOutAgendar3(){
-    $agendar3.style.bottom = '-310px';
+    agendar3.style.bottom = '-310px';
 }
 /* ----- onmouseouver perfil 4 --- */
 function hoverAgendar4(){
-    $agendar4.style.bottom = '0';
+    agendar4.style.bottom = '0';
 }
 
 function hoverOutAgendar4(){
-    $agendar4.style.bottom = '-310px';
+    agendar4.style.bottom = '-310px';
 }
 /* ----- onmouseouver perfil 5 --- */
 function hoverAgendar5(){
-    $agendar5.style.bottom = '0';
+    agendar5.style.bottom = '0';
 }
 
 function hoverOutAgendar5(){
-    $agendar5.style.bottom = '-310px';
+    agendar5.style.bottom = '-310px';
 }
 /* ----- onmouseouver perfil 6 --- */
 function hoverAgendar6(){
-    $agendar6.style.bottom = '0';
+    agendar6.style.bottom = '0';
 }
 
 function hoverOutAgendar6(){
-    $agendar6.style.bottom = '-310px';
+    agendar6.style.bottom = '-310px';
 }
 /* ----- onmouseouver perfil 7 --- */
 function hoverAgendar7(){
-    $agendar7.style.bottom = '0';
+    agendar7.style.bottom = '0';
 }
 
 function hoverOutAgendar7(){
-    $agendar7.style.bottom = '-310px';
+    agendar7.style.bottom = '-310px';
 }
 
 /*----------função que irá dar display block na tela de agendamento */
 function showMetting(){
-    $mettingSection.style.display= "block";
+    mettingSection.style.display= "block";
 }
 
 function closeMetting(){
-    $mettingSection.style.display= "none"
+    mettingSection.style.display= "none"
 }
 
 /* função para abrir o calendário na versão mobile que estará em display none */
 
 function openCalendar(){
-    $mettingData.style.display="block"
+    mettingData.style.display="block"
 }
 
 function closeCalendar(){
-    $mettingData.style.display="none"
+    mettingData.style.display="none"
 }
